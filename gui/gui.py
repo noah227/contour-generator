dd82014a7353509fc7400ac4a9ba0b694ccfc1f8
noah227 <noah227@foxmail.com>
2024-04-03 16:34:57 +0800

    chore: add test gui

---
+++ gui/gui.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# CREATED: 2024/4/3
+# AUTHOR : NOAH YOUNG
+# EMAIL  : noah227@foxmail.com
+
+import tkinter as tk
+import os
+import webview
+from main import ContourGenerator
+import time
+
+
+class JsApi:
+    def syncControls(self, threshold=100):
+        print("SYNC", threshold)
+        ContourGenerator(thresh=threshold).generateFromImage(imgPath="./110901.jpg")
+        return "./110901.svg" + f"?t={time.time()}"
+
+
+if __name__ == '__main__':
+    webview.create_window("hello", "./index.html", js_api=JsApi(), width=720, height=520)
+    webview.start(debug=True)
+    pass
