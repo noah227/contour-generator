2836be4f8a54ecc60cce049cc8922c5a51a00985
noah227 <noah227@foxmail.com>
2024-04-24 17:32:20 +0800

    chore: enhance test controls

--- gui/gui.py
+++ gui/gui.py
@@ -3,25 +3,99 @@
 # AUTHOR : NOAH YOUNG
 # EMAIL  : noah227@foxmail.com
 
-import tkinter as tk
 import os
-import webview
-from main import ContourGenerator
 import time
+import tkinter as tk
+import tkinter.filedialog
+
+import webview
+
+from main import ContourGenerator
 
 
 class JsApi:
-    def syncControls(self, threshold=100):
-        print("SYNC", threshold)
-        threshold = int(threshold)
-        ContourGenerator(thresh=threshold).generateFromImage(imgPath="./totoro.png")
-        return "./totoro.svg" + f"?t={time.time()}"
-        ContourGenerator(thresh=threshold).generateFromImage(imgPath="./110901.jpg")
-        return "./110901.svg" + f"?t={time.time()}"
+    def __init__(self):
+        # 原始输入位置
+        self.imgSource = ""
+        self.imgCurrent = ""
+        self.threshold = 100
+        self.prepare()
 
+    @staticmethod
+    def prepare():
+        if not os.path.exists("temp"):
+            os.mkdir("temp")
+        pass
+
+    def syncControls(self, data):
+        action = data.get("action")
+        threshold = data.get("threshold")
+
+        if not self.imgSource:
+            return self.renderSelectedSingleImage()
+
+        print("SYNC", threshold, self.imgSource)
+        self.threshold = int(threshold) if threshold else self.threshold
+        if action == "update":
+            return self.processImage()
+
+        elif action == "select":
+            return self.renderSelectedSingleImage()
+        elif action == "save-single":
+            return self.saveSingleImage()
+
+        # ContourGenerator(thresh=threshold).generateFromImage(imgPath="./110901.jpg")
+        # return "./110901.svg" + f"?t={time.time()}"
+
+    def renderSelectedSingleImage(self):
+        root = tk.Tk()
+        root.withdraw()
+        file = tkinter.filedialog.askopenfilename()
+        root.destroy()
+
+        if file:
+            self.imgSource = file
+            return self.processImage()
+        else:
+            return
+        pass
+
+    def processImage(self):
+        # 复制到临时文件夹
+        # 处理图片
+        ContourGenerator(thresh=self.threshold).generateFromImage(imgPath=self.imgSource, outputDir="./temp",
+                                                                  outputFileName="temp")
+        return self.processReturn()
+        pass
+
+    @staticmethod
+    def getFileExt(basename):
+        return basename.split(".")[-1]
+
+    def processReturn(self, ext="svg"):
+        self.imgCurrent = f"./temp/temp.{ext}"
+        return f"{self.imgCurrent}?t={time.time()}"
+
+    def saveSingleImage(self):
+        if self.imgCurrent:
+            root = tk.Tk()
+            root.withdraw()
+            saveFile = tkinter.filedialog.asksaveasfilename()
+            root.destroy()
+            if saveFile:
+                import shutil
+                shutil.copyfile(self.imgCurrent, saveFile)
+                pass
+        pass
 
 
 if __name__ == '__main__':
-    webview.create_window("hello", "./index.html", js_api=JsApi(), width=720, height=520)
+    webview.create_window("hello", "./index.html", js_api=JsApi(), width=960, height=600)
     webview.start(debug=True)
+    # root = tk.Tk()
+    # root.withdraw()
+    # filename = tkinter.filedialog.askopenfilename()
+    # print(filename, "<<<")
+    # print(os.path.basename("E:/Documents/test/sqllite-test/utils.py"))
+
     pass
